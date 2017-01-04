badb7dae9957b51981c639a56025147c72a8ddd8
johannes <jmuljana@gmail.com>
2017-01-04 13:20:21 +1100

    added sign up form

--- js/app.js
+++ js/app.js
@@ -1,12 +1,38 @@
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    console.log(user);
-  } else {
-    //hide the logout button
-    document.getElementById('logout').style.display='none';
-  }
+window.addEventListener('load',function(){
+  //add firebase auth observer
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log(user);
+      toggleOverlayVisibility('hide');
+    }
+    else{
+      //hide the logout button
+      document.getElementById('logout').style.display='none';
+      //show the login form
+      toggleOverlayVisibility('show');
+    }
+  });
+  document.getElementById('signup-link').addEventListener('click',showForm);
+  document.getElementById('login-link').addEventListener('click',showForm);
 });
+function showForm(evt){
+  var target = evt.target.id;
+  console.log(target);
+  if(target=='signup-link'){
+    document.getElementById('signup').style.visibility='hidden';
+    document.getElementById('login').style.visibility='visible';
+  }
+  if(target=='login-link'){
+    document.getElementById('login').style.visibility='visible';
+    document.getElementById('signup').style.visibility='hidden';
+  }
+}
 
-showLoginForm(){
-  
+function toggleOverlayVisibility(status){
+  if(status=='show'){
+    document.getElementById('overlay').style.visibility='visible';
+  }
+  if(status=='hide'){
+    document.getElementById('overlay').style.visibility='hidden';
+  }
 }
