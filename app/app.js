4f1c3031b58b7be5a6b3974981f51aae452023a5
johannes muljana <jmuljana@gmail.com>
2017-02-01 15:16:51 +0000

    getting user data upon login

--- app/app.js
+++ app/app.js
@@ -1,8 +1,11 @@
-app = {
+var app = {
+  currentuser:0,
   userstatus:0,
+  userdata:"",
   userid:"",
   database:firebase.database()
 }
+
 //things to do when window loads
 window.addEventListener('load',function(){
   //add firebase auth observer
@@ -10,20 +13,19 @@ window.addEventListener('load',function(){
     if (user) {
       //user is logged in
       //set the global user status to 1 (logged in)
-      app.userstatus = 1;
       app.userid = user.uid;
       app.user = user;
       changeNavigationStatus();
       toggleOverlayVisibility('hide');
-      //hide menu items
-      console.log(user);
+      //populate user data
+      readUserData(user.uid);
     }
     else{
       //user is logged out
-      app.userstatus = 0;
       changeNavigationStatus();
       //show the login form
       toggleOverlayVisibility('show');
+      destroyUserData();
     }
   });
   //listeners for form switcher
@@ -38,7 +40,7 @@ window.addEventListener('load',function(){
 });
 //navigation controller
 function changeNavigationStatus(){
-  if(app.userstatus == 1){
+  if(app.user){
       //show logout and profile
     document.getElementById('user-logout').style.display = 'flex';
     document.getElementById('user-profile').style.display = 'flex';
@@ -117,39 +119,41 @@ function getFormData(e){
   e.preventDefault();
   //get id from target
   var id = e.target.id;
+  //reset the form
   //get data from the form
   var formData = new FormData(document.getElementById(id));
+  e.target.reset();
   //check which form has been submitted
   if(id=='signup-form'){
     //pass form email and password to signUpUser()
-    signUpUser(formData.get('email'),formData.get('password'));
-    var username = formData.get('username');
-    //generate image
+    username = formData.get('username');
     var img = generateProfileImage(username);
-    //write user to database
-    //console.log(app.userid);
-    //writeUserData(userId, name, email, imageData);
+    signUpUser(formData.get('email'),formData.get('password'),username,img);
   }
   if(id=='login-form'){
     //pass form email and password to signInUser()
     signInUser(formData.get('email'),formData.get('password'));
   }
 }
-
-function signUpUser(email,password){
+//sign up user
+function signUpUser(email,password,username,userimage){
   firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then(function(user){
+    app.userid = user.uid;
+    writeUserData(app.userid, username, user.email, userimage);
+  })
   .catch(function(error) {
   // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
     console.log(error);
-    //write users data 
   });
-  var user = firebase.auth().currentUser;
-  //console.log(user);
+  
 }
+//sign user in
 function signInUser(email,password){
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
@@ -160,20 +164,62 @@ function signInUser(email,password){
 function signOutUser(){
   firebase.auth().signOut().then(function() {
   // Sign-out successful
-    toggleOverlayVisibility('show');
+  //set app.user property to null
+  destroyUserData();
+  showUserData();
+  changeNavigationStatus();
+  toggleOverlayVisibility('show');
   }, function(error) {
     // An error happened.
+    console.log(error);
   });
 }
+//destroy user data from app object
+function destroyUserData(){
+  app.user='';
+  app.userdata='';
+  app.userstatus = 0;
+  //remove user data elements
+  document.getElementById('username').innerHTML='';
+  document.getElementById('userimage').innerHTML='';
+}
 
-function writeUserData(userId, name, email, imageData) {
+//write user data to users branch
+function writeUserData(userId, name, email, imageData,date) {
   firebase.database().ref('users/' + userId).set({
     username: name,
     email: email,
-    profile_picture : imageData
+    profile_picture : imageData,
+    created:created
+  })
+  .then(function(result){console.log(result)});
+}
+//read user data
+function readUserData(id){
+  firebase.database().ref('/users/' + id).once('value')
+  .then(function(snapshot){
+    var username = snapshot.val().username;
+    var profile_image = snapshot.val().profile_picture;
+    var authuser = {name:username,image:profile_image};
+    app.userdata = authuser;
+    showUserData();
+  })
+  .catch(function(error){
+    console.log(error);
   });
 }
-
+function showUserData(){
+  var img = document.getElementById('userimage');
+  var name = document.getElementById('username');
+    //create user image element
+    var userimg = document.createElement('IMG');
+    userimg.setAttribute('src',app.userdata.image);
+    userimg.setAttribute('id','profileimage');
+    img.appendChild(userimg);
+    var username = document.createTextNode('Hello '+app.userdata.name);
+    name.appendChild(username);
+}
+//generate a simple profile image from username's first letter
 function generateProfileImage(username){
   var letter = username.charAt(0);
   canvas = document.createElement('CANVAS');
